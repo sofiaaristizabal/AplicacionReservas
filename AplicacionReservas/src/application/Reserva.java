c680a120b123a5cf3d1f11173bbe18dbe53f7969
Masiori <117297504+Masiori@users.noreply.github.com>
2024-02-26 10:24:02 -0500

    add reserva

---
+++ AplicacionReservas/src/application/Reserva.java
@@ -0,0 +1,59 @@
+package application;
+
+import java.util.*;
+
+public abstract class Reserva {
+	
+	protected String codigo;
+	//String de placeholder
+	protected String lugarEvento;
+	protected Date fechaGeneracion;
+	protected Date fechaReserva;
+	
+	
+	public Reserva(String codigo, String lugarEvento, Date fechaReserva) {
+		this.codigo = codigo;
+		this.lugarEvento = lugarEvento;
+		this.fechaReserva = fechaReserva;
+        generarFecha();
+  
+	}
+  
+  
+  public void generarFecha() {
+  
+	  Date d1 = new Date();
+
+		this.fechaGeneracion = d1;
+		
+  }
+
+
+	public String getCodigo() {
+		return codigo;
+	}
+
+
+	public String getLugarEvento() {
+		return lugarEvento;
+	}
+
+
+	public Date getFechaGeneracion() {
+		return fechaGeneracion;
+	}
+
+
+	public Date getFechaReserva() {
+		return fechaReserva;
+	}
+
+	
+
+
+	
+	
+	
+	
+
+}
