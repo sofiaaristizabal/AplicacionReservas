3864f29ee11ee5f642c5d1e5fbd3a91c364c1105
sofia <sofiaaristizabal23@gmail.com>
2024-03-03 15:06:30 -0500

    corregui error

--- AplicacionReservas/src/application/EmpresaSonido.java
+++ AplicacionReservas/src/application/EmpresaSonido.java
@@ -5,8 +5,8 @@ public class EmpresaSonido extends EmpresaPrestadoraServicio {
     private String[] marcaEquipo;
 
     // Constructor
-    public EmpresaSonido(String nombre, String codigo, String[] tipoGenero,double presupuesto, String[] marcaEquipo) {
-        super(nombre, codigo, presupuesto);
+    public EmpresaSonido(String nombre, String codigo, String[] tipoGenero, TarifasOfrecidas tarifasOfrecidas, String[] marcaEquipo) {
+        super(nombre, codigo, tarifasOfrecidas);
         this.tipoGenero = tipoGenero;
         this.marcaEquipo = marcaEquipo;
     }
