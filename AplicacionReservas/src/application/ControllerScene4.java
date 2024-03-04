c75f723ad9d493c9007d972fca0303151a06cae1
sofia <sofiaaristizabal23@gmail.com>
2024-03-04 09:21:14 -0500

    interfaz

---
+++ AplicacionReservas/src/application/ControllerScene4.java
@@ -0,0 +1,96 @@
+package application;
+
+import java.net.URL;
+import java.time.LocalDate;
+import java.util.ResourceBundle;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.ChoiceBox;
+import javafx.scene.control.DatePicker;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextField;
+
+public class ControllerScene4 implements Initializable{
+
+	@FXML
+	private Label variableLabel;
+	
+	@FXML
+	private ChoiceBox<String> tipoReserva;
+	
+	@FXML
+	private DatePicker seleccionarFecha;
+	
+	@FXML
+	private Label fecha;
+	
+	@FXML
+	private ChoiceBox<String> lugaresEventos;
+	
+    @FXML
+	TextField nameTextField;
+    
+    @FXML
+    private Button reservar;
+	
+	private Aplicacion app = Aplicacion.getAplicacion();
+	private LugarDeEvento [] lugares = app.getLugaresEventos();
+	private String [] nombres;
+	
+	public void generarReserva() {
+		
+		
+	}
+	
+	private String[] tiposReservas = {"Reserva de lugar", "Reserva de visita"};
+	
+    public void obtenerNombres() {
+		
+		nombres = new String[lugares.length];
+		for(int i = 0; i < lugares.length; i++) {
+			nombres[i] = lugares[i].getNombre();
+		}
+	}
+
+	@Override
+	public void initialize(URL arg0, ResourceBundle arg1) {
+		
+		initialize1();
+		initialize2();
+	}
+	
+	public void initialize1() {
+		
+        tipoReserva.getItems().addAll(tiposReservas);
+		
+		tipoReserva.setOnAction(this::getReserva);
+	}
+    public void initialize2() {
+		obtenerNombres();
+		lugaresEventos.getItems().addAll(nombres);
+	}
+	
+	
+	
+	public void getReserva(ActionEvent event) {
+		
+		String reserva = tipoReserva.getValue();
+		
+		if(reserva.equals("Reserva de lugar")) {
+			variableLabel.setText("Cantidad de personas para el evento");
+		} else {
+			variableLabel.setText("Hora de visita");
+		}
+	}
+	
+	public void getDate(ActionEvent event) {
+		
+		LocalDate myDate = seleccionarFecha.getValue();
+		
+		fecha.setText(myDate.toString());
+	}
+	
+}
